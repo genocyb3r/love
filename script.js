5da1237e076366c4658e0c2124f8da95ff7e1ceb
genocybe3r <donugaclent@gmail.com>
2024-11-24 10:18:28 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element by its ID
+const button = document.getElementById('redirect-btn');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    // Redirect to another page (replace 'nextpage.html' with the actual URL)
+    window.location.href = 'nextpage.html';  // You can change this to your desired URL
+});
